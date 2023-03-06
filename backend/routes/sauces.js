5f1a834d3df3307e8321281e79deae33d9a9401b
Blackophelie <blackophelie@hotmail.com>
2023-03-06 12:22:22 +0100

    Affichage photos et sauces OK

---
+++ backend/routes/sauces.js
@@ -0,0 +1,24 @@
+// ----------------Importations----------------------
+
+const express = require("express");
+
+// Contrôleur sauce
+const sauceCtrl = require("../controllers/sauces");
+// const likeCtrl = require("../controllers/sauces");
+// Middleware d'authentification
+const auth = require("../middleware/auth");
+// Middleware multer pour gestion des fichiers image
+const multer = require("../middleware/multer-config");
+
+// Méthode d'express Router()
+const router = express.Router();
+
+// Routes
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.get("/", sauceCtrl.getAllSauces);
+router.post("/:id/like", auth, sauceCtrl.didUserlike);
+
+module.exports = router;
